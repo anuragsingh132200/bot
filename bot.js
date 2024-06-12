eea1c3c3991bfe8d3408d1bdc298d1d66eee1f8c
Anuragsingh132200 <anurag132200@gmail.com>
2024-06-12 06:21:36 +0530

    minor updates

--- bot.js
+++ bot.js
@@ -1,42 +1,53 @@
 const venom = require("venom-bot");
 const searchMedicine = require("./scrappers/netmeds");
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  venom
+    .create({
+      session: "session-name", //name of session
+    })
+    .then((client) => start(client))
+    .catch((erro) => {
+      console.log(erro);
+    });
 
+  function start(client) {
+    client.onMessage((message) => {
+      if (message.body === "Hi" && message.isGroupMsg === false) {
+        client
+          .sendText(message.from, "Welcome to Medicare")
+          .then((result) => {
+            console.log("Result: ", result); //return object success
+          })
+          .catch((erro) => {
+            console.error("Error when sending: ", erro); //return object error
+          });
+      } else {
+        searchMedicine(message.body)
+          .then((result) => {
+            console.log(result);
+            const responseMessage = `This is the medicine name ${result.productName}, at price ₹${result.productPrice}, at link ${result.productLink},on ${result.siteName} brand`;
+            client
+              .sendText(message.from, responseMessage)
+              .then((result) => {
+                console.log("Result: ", result); //return object success
+              })
+              .catch((erro) => {
+                console.error("Error when sending: ", erro); //return object error
+              });
+          })
+          .catch((error) => console.error(error));
+      }
+    });
+  }
 
-venom
-  .create({
-    session: "session-name", //name of session
-  })
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
-  });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!");
+});
 
-function start(client) {
-  client.onMessage((message) => {
-    if (message.body === "Hi" && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, "Welcome to Medicare")
-        .then((result) => {
-          console.log("Result: ", result); //return object success
-        })
-        .catch((erro) => {
-          console.error("Error when sending: ", erro); //return object error
-        });
-    } else {
-      searchMedicine(message.body)
-        .then((result) => {
-          console.log(result);
-          const responseMessage = `This is the medicine name ${result.productName}, at price ₹${result.productPrice}, at link ${result.productLink},on ${result.siteName} brand`;
-          client.sendText(message.from, responseMessage)
-            .then((result) => {
-              console.log("Result: ", result); //return object success
-            })
-            .catch((erro) => {
-              console.error("Error when sending: ", erro); //return object error
-            });
-        })
-        .catch((error) => console.error(error));
-    }
-  });
-}
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
